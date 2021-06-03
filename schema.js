477b2170d3473aae24b34f0ae02660d0bff64e81
mi4en <marionikolov87@gmail.com>
2021-06-03 08:19:54 +0300

    build query tpes and fetch data;

--- schema.js
+++ schema.js
@@ -7,4 +7,65 @@ const {
 	GraphQLNonNull,
 } = require('graphql')
 
-module.exports = new GraphQLSchema({})
+// Mock data
+const customers = [
+	{
+		id: '1',
+		name: 'Liu Kang',
+		email: 'kang@earthrealm.com',
+		age: 25,
+	},
+	{
+		id: '2',
+		name: 'Sonya Blade',
+		email: 'blade@earthrealm.com',
+		age: 27,
+	},
+	{
+		id: '3',
+		name: 'Kung Lao',
+		email: 'lao@earthrealm.com',
+		age: 32,
+	},
+]
+
+// Create Customer Type
+const CustomerType = new GraphQLObjectType({
+	name: 'Customer',
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		email: { type: GraphQLString },
+		age: { type: GraphQLInt },
+	}),
+})
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		customer: {
+			type: CustomerType,
+			args: {
+				id: { type: GraphQLString },
+			},
+			resolve(parentValue, args) {
+				for (let i = 0; i, customers.length; i++) {
+					if (customers[i].id === args.id) {
+						return customers[i]
+					}
+				}
+			},
+		},
+		customers: {
+			type: new GraphQLList(CustomerType),
+			resolve(parentValue) {
+				return customers
+			},
+		},
+	},
+})
+
+module.exports = new GraphQLSchema({
+	query: RootQuery,
+})
